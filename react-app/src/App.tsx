b8ede02126f3dee017badf0197114a8595f199ff
Mahekaru Hitokui <mahekaru@gmail.com>
2025-09-13 20:14:09 -0400

    Updating

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -61,6 +61,18 @@ function App() {
     })
   }
 
+  const updateUser = (user: User) => {
+    const originalUsers = [...users];
+    const updatedUser = {...user, name: user.name + "!"};
+    setUsers(users.map(u => u.id === user.id ? updatedUser : u));
+
+    axios.patch(`https://jsonplaceholder.typicode.com/users/${user.id}`, updatedUser)
+    .catch(err => {
+      setError(err.message);
+      setUsers(originalUsers);
+    })
+  }
+
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
@@ -70,7 +82,11 @@ function App() {
         {users.map((user) => (
           <li className="list-group-item d-flex justify-content-between" key={user.id}>
             {user.name}
+            <div>
+            <button className="btn btn-outline-secondary mx-1" onClick={() => updateUser(user)}>Update</button>
             <button className="btn btn-outline-danger" onClick={() => deleteUser(user)}>Delete</button>
+            </div>
+
           </li>
         ))}
       </ul>
