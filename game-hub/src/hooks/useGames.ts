1afad22babe903aabc31446d8f927670b7f22008
Mahekaru Hitokui <mahekaru@gmail.com>
2025-10-05 16:21:42 -0400

    gamecardskeleton

--- game-hub/src/hooks/useGames.ts
+++ game-hub/src/hooks/useGames.ts
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
+import { set } from "react-hook-form";
 
-export interface Platform{
-    id: number;
-    name: string;
-    slug: string;
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
 }
 export interface Game {
   id: number;
@@ -23,22 +24,28 @@ interface FetchGamesResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
     const controller = new AbortController();
 
+    setLoading(true);
     apiClient
       .get<FetchGamesResponse>("/games", { signal: controller.signal })
-      .then((res) => setGames(res.data.results))
+      .then((res) => {
+        setGames(res.data.results);
+        setLoading(false);
+      })
       .catch((err) => {
-        if(err instanceof CanceledError) return;
+        if (err instanceof CanceledError) return;
         setError(err.message)
-    });
+        setLoading(false);
+      });
 
-      return () => controller.abort();
+    return () => controller.abort();
   }, []);
 
-  return { games, error };
-}
+  return { games, error, isLoading };
+};
 
-export default useGames
+export default useGames;
