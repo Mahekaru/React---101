80f168c16af763aba2d558c1cbd132f886c48a2d
Mahekaru Hitokui <mahekaru@gmail.com>
2025-10-18 13:09:09 -0400

    Adding sort dropdown

---
+++ game-hub/src/components/SortSelector.tsx
@@ -0,0 +1,24 @@
+import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react';
+import { data } from 'framer-motion/client';
+
+import { BsChevronDown } from 'react-icons/bs';
+
+function SortSelector() {
+  return (
+      <Menu>
+        <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+          Order by: relevance
+        </MenuButton>
+        <MenuList>
+            <MenuItem>Relavance</MenuItem>
+            <MenuItem>Date Added</MenuItem>
+            <MenuItem>Name</MenuItem>
+            <MenuItem>Release Date</MenuItem>
+            <MenuItem>Popularity</MenuItem>
+            <MenuItem>Average Rating</MenuItem>
+        </MenuList>
+      </Menu>
+    );
+}
+
+export default SortSelector
